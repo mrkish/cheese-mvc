a0fcc52874be7102538875a1e3caa2217b931354
mrkish <mark@jhns.in>
2018-05-18 08:28:38 -0500

    Fixed weird ghost cheese error, where index wouldn't display cheeses even though the jobs had them and would show them on other pages.

--- src/main/java/com/com/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/com/cheesemvc/controllers/CheeseController.java
@@ -6,20 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+    static HashMap<String, String> cheeses = new HashMap<>();
 
     // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
 
-        model.addAttribute("cheese", cheeses);
+        model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My cheeses");
 
         return "cheese/index";
@@ -32,8 +32,26 @@ public class CheeseController {
     }
 
     @RequestMapping(value="add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName) {
-        cheeses.add(cheeseName);
-        return "redirect";
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String description) {
+        cheeses.put(cheeseName, description);
+        return "redirect:";
     }
+
+    @RequestMapping(value="remove", method=RequestMethod.GET)
+    public String removeCheeseForm(Model model) {
+        model.addAttribute("title", "Remove cheese");
+        model.addAttribute("cheeses", cheeses);
+
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value="remove", method=RequestMethod.POST)
+    public String processRemoveCheese(@RequestParam ArrayList<String> cheeseToDelete) {
+        for(String c : cheeseToDelete) {
+            cheeses.remove(c);
+        }
+
+        return "redirect:";
+    }
+
 }
